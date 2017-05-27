75862e277c411d4871ae4d1b75568d5e07d21832
divo <sdiviney@zendesk.com>
2017-05-27 19:22:48 +0100

    adding a bar chart to iOS

--- d3-swift/d3-swift/js/test.js
+++ d3-swift/d3-swift/js/test.js
@@ -20,3 +20,40 @@ var getPath = function(data, dim) {
   
   return line(data);
 }
+
+var document = ""
+
+var barChart = function(data, dim) {
+    const [w, h] = dim;
+    const barPadding = 1;
+    
+    var outputSVG = document.createElementNS(d3.namespaces.svg, 'svg')
+    var svg = d3.select(outputSVG)
+                .attr("width", w)
+                .attr("height", h);
+    
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) { return i * (w / data.length); })
+        .attr("y", function(d) { return h - (d * 4); })
+        .attr("width", w / data.length - barPadding)
+        .attr("height", function(d) { return d * 4; })
+        .attr("fill", "teal");
+    
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(function(d) { return d; })
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i) {
+              return i * (w / data.length) + (w / data.length - barPadding) / 2;
+          })
+        .attr("y", function(d) { return h - (d * 4) + 14; })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+    
+}
